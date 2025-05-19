e2698b5bb3410547074f0ecc54d6d40988530b0e
AnnaM135 <anna.melqonian@gmail.com>
2025-05-19 09:04:15 +0400

    feat: create header file for all classes

--- src/main.h
+++ src/main.h
@@ -1,3 +1,60 @@
 #include <string>
 
-int main();
+class Organization {
+public:
+    std::string name;
+    std::string type;
+    std::string registrationNumber;
+    std::string address;
+    std::string phone;
+    std::string email;
+    std::string website;
+    std::string director;
+    double monthlyBudget;
+
+    virtual void printInfo();
+    virtual double calculateAnnualBudget();
+};
+
+class Foundation : public Organization {
+public:
+    void printInfo() override;
+};
+
+class NGO : public Organization {
+public:
+    void printInfo() override;
+};
+
+class LLC : public Organization {
+public:
+    void printInfo() override;
+};
+
+class JSC : public Organization {
+public:
+    void printInfo() override;
+};
+
+class Retail : public LLC {
+public:
+    int numberOfProducts;
+    std::string mainCategory;
+
+    void printInfo() override;
+    double calculateAnnualBudget() override;
+};
+
+class Education : public Foundation {
+public:
+    int numberOfStudents;
+    std::string educationLevel;
+
+    void printInfo() override;
+    double calculateAnnualBudget() override;
+};
+
+class Service : public LLC {
+    // No additional methods or members yet
+};
+
